428bbc4a9ade3b9d846180ea029cf7364d425d92
JuanDavila1101 <tatojuan713@hotmail.com>
2021-05-04 16:14:51 -0500

    Ading cards with Prop types to the authors

--- src/helpers/data/authorsData.js
+++ src/helpers/data/authorsData.js
@@ -3,18 +3,24 @@ import firebaseConfig from '../apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
 
+const getAuthors = () => new Promise((resolve, reject) => {
+  // console.warn(`${dbUrl}/authors.json`);
+  // debugger;
+  axios.get(`${dbUrl}/authors.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
 // add AUTHOR
 const addAuthor = (authorObject) => new Promise((resolve, reject) => {
-  console.warn(`${dbUrl}/authors.json`);
-  debugger;
   axios.post(`${dbUrl}/authors.json`, authorObject)
     .then((response) => {
       const body = { firebaseKey: response.data.name };
       axios.patch(`${dbUrl}/authors/${response.data.name}.json`, body)
         .then(() => {
-          // getAuthors(uid).then((authorsArray) => resolve(authorsArray));
+          getAuthors().then((authorsArray) => resolve(authorsArray));
         });
     }).catch((error) => reject(error));
 });
 
-export default addAuthor;
+export { addAuthor, getAuthors };
